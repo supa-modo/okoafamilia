3fc5b094006ac804a46433ac696215082d2e7beb
supa-modo <eddieochieng11@gmail.com>
2025-12-21 11:54:53 +0300

    Add @vercel/analytics dependency and integrate analytics component in App

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,18 @@ import { Routes, Route } from "react-router-dom";
 import LandingPage from "./pages/LandingPage";
 import RegisterPage from "./pages/RegisterPage";
 import PaymentPage from "./pages/PaymentPage";
+import { Analytics } from "@vercel/analytics/react"
 
 function App() {
   return (
-    <Routes>
+    <>
+      <Analytics />
+      <Routes>
       <Route path="/" element={<LandingPage />} />
       <Route path="/register" element={<RegisterPage />} />
       <Route path="/pay" element={<PaymentPage />} />
-    </Routes>
+      </Routes>
+    </>
   );
 }
 
